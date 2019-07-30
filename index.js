46a086d9f4f7bd9cd29fc4f68e8556cbcab94b28
Daniel Burke <danieljburke@me.com>
2019-07-30 21:01:26 +1000

    using express

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer } = require('apollo-server-express')
 const { GraphQLScalarType } = require('graphql')
+const express = require('express')
+const expressPlayground = require('graphql-playground-middleware-express').default
 
 const typeDefs = `
 	scalar DateTime
@@ -137,11 +139,17 @@ const resolvers = {
 	})
 }
 
+var app = express();
+
 const server = new ApolloServer({
 	typeDefs,
 	resolvers
-})
+});
 
-server
-	.listen()
-	.then(({url}) => console.log(`GraphQL Server running on ${url}`))
+server.applyMiddleware({ app })
+
+app.get('/', (request, response) => response.end('Welcome to the PhotoShare API'));
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+app.listen({ port: 4000 }, () =>
+	console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
+);
